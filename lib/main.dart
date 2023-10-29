357d482abbde0626821eb3e6395276f363ee2d05
BuckBeatable <unbeatable1687@gmail.com>
2023-10-29 14:26:00 +0900

    amplify main test

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,222 @@
-import 'package:bpro_loan_calculator/pages/calculator/calculate_area.dart';
-import 'package:bpro_loan_calculator/pages/calculator/graph_area.dart';
-import 'package:bpro_loan_calculator/pages/calculator/manual_area.dart';
-import 'package:bpro_loan_calculator/pages/calculator/summary_area.dart';
+  import 'package:amplify_api/amplify_api.dart';
+  import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+  import 'package:amplify_authenticator/amplify_authenticator.dart';
+  import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(MyApp());
+  import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
+  runApp(const MyApp());
+}
+
+Future<void> _configureAmplify() async {
+   try {
+     // Create the API plugin.
+     //
+     // If `ModelProvider.instance` is not available, try running 
+     // `amplify codegen models` from the root of your project.
+     final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+ 
+     // Create the Auth plugin.
+     final auth = AmplifyAuthCognito();
+ 
+     // Add the plugins and configure Amplify for your app.
+     await Amplify.addPlugins([api, auth]);
+     await Amplify.configure(amplifyconfig);
+ 
+     safePrint('Successfully configured');
+   } on Exception catch (e) {
+     safePrint('Error configuring Amplify: $e');
+   }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // GoRouter configuration
+  static final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+      ),
+    ],
+  );
+
+@override
+  Widget build(BuildContext context) {
+     return Authenticator(
+       child: MaterialApp.router(
+         routerConfig: _router,
+         debugShowCheckedModeBanner: false,
+         builder: Authenticator.builder(),
+       ),
+     );
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      //home: MyHomePage(),
-      routes: {
-        '/': (context) => MyHomePage(),
-        '/detail': (context) => GraphArea(),
-      },
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  var borderBackGroundColor = Colors.black;
-  var mainBackGroundColor = Color.fromARGB(255, 99, 94, 94);
-  var subTitleColor = Colors.lightBlue;
-  var textColor = Colors.white;
-  var titleBold = FontWeight.w600;
-  var subTitleBold = FontWeight.w300;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-  MyHomePage({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _budgetEntries = <BudgetEntry>[];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _refreshBudgetEntries() async {
+    // To be filled in
+  }
+
+  Future<void> _deleteBudgetEntry(BudgetEntry budgetEntry) async {
+    // To be filled in
+  }
+
+  Future<void> _navigateToBudgetEntry({BudgetEntry? budgetEntry}) async {
+    // To be filled in
+  }
+
+  double _calculateTotalBudget(List<BudgetEntry?> items) {
+    var totalAmount = 0.0;
+    for (final item in items) {
+      totalAmount += item?.amount ?? 0;
+    }
+    return totalAmount;
+  }
+
+  Widget _buildRow({
+    required String title,
+    required String description,
+    required String amount,
+    TextStyle? style,
+  }) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: style,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            description,
+            textAlign: TextAlign.center,
+            style: style,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            amount,
+            textAlign: TextAlign.center,
+            style: style,
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          //padding: EdgeInsets.only(left: 16.0),
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "Loan Calculator",
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: titleBold,
-            ),
-          ),
-        ),
-        backgroundColor: borderBackGroundColor,
-        toolbarHeight: 100,
+      floatingActionButton: FloatingActionButton(
+        // Navigate to the page to create new budget entries
+        onPressed: _navigateToBudgetEntry,
+        child: const Icon(Icons.add),
       ),
-      body: Row(
-        children: [
-          // 좌측 영역
-          Expanded(
-            flex: 1, // 1/6 비율
-            child: Container(
-              color: borderBackGroundColor,
-              child: Center(
-                child: Text(
-                  '대출이자계산기',
-                  style: TextStyle(
-                    color: textColor,
-                    fontSize: 20,
-                    fontWeight: subTitleBold,
+      appBar: AppBar(
+        title: const Text('Budget Tracker'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: RefreshIndicator(
+            onRefresh: _refreshBudgetEntries,
+            child: Column(
+              children: [
+                if (_budgetEntries.isEmpty)
+                  const Text('Use the \u002b sign to add new budget entries')
+                else
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Show total budget from the list of all BudgetEntries
+                      Text(
+                        'Total Budget: \$ ${_calculateTotalBudget(_budgetEntries).toStringAsFixed(2)}',
+                        style: const TextStyle(fontSize: 24),
+                      )
+                    ],
                   ),
+                const SizedBox(height: 30),
+                _buildRow(
+                  title: 'Title',
+                  description: 'Description',
+                  amount: 'Amount',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
-              ),
-            ),
-          ),
-          // 우측 영역
-          Expanded(
-            flex: 5, // 5/6 비율
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  CalculateArea(backgroundColor: mainBackGroundColor, titleColor: subTitleColor, textColor: textColor),
-                  SummaryArea(backgroundColor: mainBackGroundColor, titleColor: subTitleColor, textColor: textColor),
-                  //회차, 상환금, 상환금액(지급금액), 이자, 납입원금 계, 잔금
-                  Container(
-                    color: mainBackGroundColor,
-                    padding: EdgeInsets.only(
-                        left: 50, right: 50, bottom: 30, top: 30),
-                    child: Column(
-                      children: [
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Help",
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w600,
-                              color: subTitleColor,
+                const Divider(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _budgetEntries.length,
+                    itemBuilder: (context, index) {
+                      final budgetEntry = _budgetEntries[index];
+                      return Dismissible(
+                        key: ValueKey(budgetEntry),
+                        background: const ColoredBox(
+                          color: Colors.red,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Icon(Icons.delete, color: Colors.white),
                             ),
                           ),
                         ),
-                        SizedBox(height: 20),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                        onDismissed: (_) => _deleteBudgetEntry(budgetEntry),
+                        child: ListTile(
+                          onTap: () => _navigateToBudgetEntry(
+                            budgetEntry: budgetEntry,
+                          ),
+                          title: _buildRow(
+                            title: budgetEntry.title,
+                            description: budgetEntry.description ?? '',
+                            amount:
+                                '\$ ${budgetEntry.amount.toStringAsFixed(2)}',
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
